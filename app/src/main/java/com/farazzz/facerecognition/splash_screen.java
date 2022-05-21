7f0d9c3083b71c189bd0dae6233cd1b5403299fb
farazxsiddiqui <ahmedfarazsiddiqui1@gmail.com>
2022-05-22 01:06:57 +0530

    Initial commit

---
+++ app/src/main/java/com/farazzz/facerecognition/splash_screen.java
@@ -0,0 +1,25 @@
+package com.farazzz.facerecognition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent =new Intent(splash_screen.this,MainActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        },2000);
+    }
+}
